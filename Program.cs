cd703c58b65d6496bef1b23b361152f41132f41b
Deepak <deepak@gmail.com>
2023-05-25 13:02:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace learningArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = { "kathir", "vicky", "hari", "nicky", "john" };
+            foreach (string friend in friends)
+            {
+                Console.WriteLine("Welcome {0}",friend);
+            }
+        }
+    }
+}
